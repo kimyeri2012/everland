8f396a2f6701feb2c378dc064a8bccde937e3b75
kimyeri2012 <kimyeri2012@gmail.com>
2022-11-14 16:22:28 +0900

    로그인 누락

---
+++ web_project/login/logout.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+// 세션 삭제
+// unset(세션변수);
+unset($_SESSION["s_idx"]);
+unset($_SESSION["s_name"]);
+unset($_SESSION["s_id"]);
+
+// 페이지 이동
+echo "
+    <script type=\"text/javascript\">
+        location.href=\"../index.php\";
+    </script>
+";
+?>
