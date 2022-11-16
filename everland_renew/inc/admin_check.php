2ecb8b5c8dba2166818919be38e67d233d48ec3c
kimyeri2012 <kimyeri2012@gmail.com>
2022-11-16 18:00:38 +0900

    관리자 로그인 체크 include

---
+++ everland_renew/inc/admin_check.php
@@ -0,0 +1,12 @@
+<?php
+header("Content-Type: text/html; charset=UTF-8"); 
+if(!$s_idx || $s_id!="admin"){
+    echo "
+        <script type=\"text/javascript\">
+            alert(\"관리자 로그인이 필요합니다.\");
+            location.href = \"http://localhost/web_project/login/login.php\";
+        </script>
+    ";
+    exit;
+};
+?>
