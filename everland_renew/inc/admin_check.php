90d609b818713142d5a19da6c48894e195160079
kimyeri2012 <kimyeri2012@gmail.com>
2022-11-18 18:15:01 +0900

    주소 수정

--- everland_renew/inc/admin_check.php
+++ everland_renew/inc/admin_check.php
@@ -4,7 +4,7 @@ if(!$s_idx || $s_id!="admin"){
     echo "
         <script type=\"text/javascript\">
             alert(\"관리자 로그인이 필요합니다.\");
-            location.href = \"http://localhost/web_project/login/login.php\";
+            location.href = \"http://localhost/everland_renew/login/login.php\";
         </script>
     ";
     exit;
